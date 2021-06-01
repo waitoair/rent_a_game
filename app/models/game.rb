819641ae1ba1707e0ee996a97726ee1c8d3fcb9e
lara barbosa <larapattylprb@gmail.com>
2021-06-01 14:12:54 -0300

    game:index/show - user:devise

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :rents, dependent: :destroy
   validates :name, presence: true
-  validates :description, presence: true, mininum: { length: 10 }
+  validates :description, presence: true, length: { minimum: 10 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true
 end
