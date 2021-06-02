f909e6d28c402f707e5f2a28d3104198264c6d25
RodrigoDonato <rodrigosdonato@outlook.com>
2021-06-02 14:48:08 -0400

    rent view

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,12 +1,29 @@
 class RentsController < ApplicationController
+  def show
+    @rent = Rent.find(params[:id])
+  end
+
   def new
+    @game = Game.find(params[:game_id])
     @rent = Rent.new
   end
 
   def create
     @rent = Rent.new(rent_params)
+    @rent.game = Game.find(params[:game_id])
+    @rent.user = current_user
     @rent.save
-    redirect_to game_path
+    redirect_to @rent
+  end
+
+  def update
+    @rent = Rent.find(params[:id])
+    @rent.update(rent_params)
+    redirect_to @rent
+  end
+
+  def edit
+    @rent = Rent.find(params[:id])
   end
 
   def destroy
@@ -18,6 +35,6 @@ class RentsController < ApplicationController
   private
 
   def rent_params
-    params.require(:rent).permit(:total_days)
+    params.require(:rent).permit(:rent_world, :rent_game, :return_date)
   end
 end
