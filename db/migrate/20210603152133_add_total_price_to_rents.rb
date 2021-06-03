bc8e53542e13f3d18d3d2df4e966c5b1c8dec386
RodrigoDonato <rodrigosdonato@outlook.com>
2021-06-03 16:21:01 -0400

    tatal price available

--- db/migrate/20210603152133_add_total_price_to_rents.rb
+++
@@ -1,6 +0,0 @@
-class AddTotalPriceToRents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :rents, :total_price, :float
-    add_column :rents, :total_days, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_152133) do
+ActiveRecord::Schema.define(version: 2021_05_31_204440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "controllers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_controllers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
-  end
-
   create_table "games", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.float "price"
-    t.string "category"
-    t.boolean "world"
-    t.boolean "available"
+    t.string "name", default: "", null: false
+    t.text "description", default: "", null: false
+    t.decimal "price", default: "0.0", null: false
+    t.string "category", default: "", null: false
+    t.boolean "world", default: false, null: false
+    t.boolean "available", default: true, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -43,13 +31,13 @@ ActiveRecord::Schema.define(version: 2021_06_03_152133) do
   create_table "rents", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "game_id", null: false
-    t.boolean "rent_world"
-    t.boolean "rent_game"
-    t.date "return_date"
+    t.boolean "rent_world", default: false, null: false
+    t.boolean "rent_game", default: false, null: false
+    t.date "return_date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.decimal "total_price", default: "0.0", null: false
+    t.integer "total_days", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "total_price"
-    t.integer "total_days"
     t.index ["game_id"], name: "index_rents_on_game_id"
     t.index ["user_id"], name: "index_rents_on_user_id"
   end
@@ -57,8 +45,8 @@ ActiveRecord::Schema.define(version: 2021_06_03_152133) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "phone_number"
-    t.string "name"
+    t.string "phone_number", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
