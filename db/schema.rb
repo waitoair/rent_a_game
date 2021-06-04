74451a7053a57aa97183a80ad21f394eb65d2227
lara barbosa <larapattylprb@gmail.com>
2021-06-04 10:58:43 -0300

    controllers

--- db/schema.rb
+++ db/schema.rb
@@ -36,18 +36,6 @@ ActiveRecord::Schema.define(version: 2021_06_03_145202) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "controllers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_controllers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
-  end
-
   create_table "games", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.text "description", default: "", null: false
